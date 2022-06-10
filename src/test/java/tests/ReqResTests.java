0a7c665945d4349dc09f4dde04e4573367cf9f92
myrisingsun <kpaharev@gmail.com>
2022-06-10 20:04:23 +0300

    Final - all ReqRes test added

--- src/test/java/tests/ReqResTests.java
+++ src/test/java/tests/ReqResTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class ReqResTests {
 
@@ -42,6 +42,57 @@ public class ReqResTests {
                 .body("job", is("leader"));
     }
 
+    @Test
+    void registerNewUserSucess() {
+        String body = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
+
+        given()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("id", is(4))
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+@Test
+ void registerNewUserUnsucess(){
+
+    String body = "{ \"email\": \"sydney@fife\" }";
+            given()
+                    .log().all()
+                    .contentType(JSON)
+                    .body(body)
+                    .when()
+                    .post("https://reqres.in/api/register")
+                    .then()
+                    .log().all()
+                    .statusCode(400)
+                    .body("error", is("Missing password"));
+        }
+
+        @Test
+
+    void loginUserWrongPassword(){
+        String body = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+            given()
+                    .log().all()
+                    .contentType(JSON)
+                    .body(body)
+                    .when()
+                    .put("https://reqres.in/api/users/2")
+                    .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("name", is("morpheus"))
+                    .body("job", is("zion resident"));
+
+
+            }
+
 
 
     }
+
